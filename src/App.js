cbcafbb0713b6c2314b1af505ac27985b503788e
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-09-05 17:33:18 +0530

    Mobile design

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ function App() {
   return (
     <div>
       <nav className='text-center p-0 md:flex md:justify-around md:p-0 bg-secclr'>
-      <img src="   https://cdn-icons-png.flaticon.com/512/10381/10381028.png " alt='Suhas Koheda' className='rounded-full w-10 h-8 object-cover fixed mr-0 right-0 md:hidden' onClick={!handleToggle}/>
+      <img src="https://img.icons8.com/?size=256&id=S5biqohaDgd1&format=png" alt='Suhas Koheda' className='rounded-full w-10 h-8 object-cover fixed mr-0 right-0 md:hidden' onClick={handleToggle}/>
         <div><h3 className={`${isVisible && window.innerWidth<768 ? 'fixed' : ''} text-2xl font-serif bg-secclr`}>Suhas Koheda</h3></div>
         {isVisible && (
       <div className={`${isVisible && window.innerWidth < 768 ? 'fixed w-screen h-screen' : ''} text-center p-12 md:flex md:justify-end gap-28 md:p-0 bg-secclr`}>
+                    <div className="p-8 md:p-0">
+                        <a href="#" className="bg-secclr" onClick={handleToggle}>Home</a>
+                    </div>
                     <div className="p-8 md:p-0">
                         <a href="#About" className="bg-secclr" onClick={handleToggle}>About Me</a>
                     </div>
